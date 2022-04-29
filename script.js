3ff944ee109f4586fe689ef84f3e0c7378deb02a
Phlixx <you@example.com>
2022-04-29 18:30:23 +0000

    fulfilling the objectives

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    const p = document.getElementById("principal").value;
+    const r = document.getElementById("rate").value;
+    const years = document.getElementById("years").value;
+    const year = new Date().getFullYear();
+    const futureYear = Number(years)+Number(year);
+    const result = p*(Number(r)/100*Number(years)+1)-p;
     
+    var resultElement = document.getElementById("result");
+
+    var resultText = '<br>If you deposit ' + p + ',<br> at an interest rate of ' + r +'%. <br> You will receive an amount of ' + result + ',<br> in the year ' + futureYear;
+    resultElement.innerHTML=resultText;
+}
+function displayRate()
+{
+    var rateDisplay = document.getElementById("rateDisplay");
+    const r = document.getElementById("rate").value;
+
+    rateDisplay.innerHTML = r + "%";
+
 }
         
